f6daca91509220ecf30e02fbc0498f9f4a0e3508
Philippe Massicotte <pmassicotte@hotmail.com>
2021-08-23 09:54:42 -0400

    Convert POC from umol to mg.

--- R/fig07.R
+++ R/fig07.R
@@ -6,13 +6,16 @@
 
 rm(list = ls())
 
+# Do not forget to convert umol L to mg m3.
+# 1 molC = 12 gC
 poc <- read_csv(here("data","clean","poc.csv")) %>%
-  select(station, transect, depth_m, poc_umol_l)
+  select(station, transect, depth_m, poc_umol_l) %>%
+  mutate(poc_mg_m3 = ((poc_umol_l / 0.000001) * 12) / 1000000)
 
 # Will use this to have the same scale on the y-axes
 
-min_poc <- min(poc$poc_umol_l, na.rm = TRUE) * 0.5
-max_poc <- max(poc$poc_umol_l, na.rm = TRUE) * 1.5
+min_poc <- min(poc$poc_mg_m3, na.rm = TRUE) * 0.5
+max_poc <- max(poc$poc_mg_m3, na.rm = TRUE) * 1.5
 
 # POC vs CP ---------------------------------------------------------------
 
@@ -31,10 +34,10 @@ poc_cp <- poc_cp %>%
   filter(depth_m <= 100)
 
 p1 <- poc_cp %>%
-  ggplot(aes(x = cp, y = poc_umol_l)) +
+  ggplot(aes(x = cp, y = poc_mg_m3)) +
   geom_point(color = "#393E41") +
-  ggpubr::stat_regline_equation(label.y = log10(80)) +
-  ggpubr::stat_regline_equation(label.y = log10(50), aes(label = ..rr.label..)) +
+  ggpubr::stat_regline_equation(label.y.npc = 1) +
+  ggpubr::stat_regline_equation(label.y.npc = 0.93, aes(label = ..rr.label..)) +
   scale_y_log10(limits = c(min_poc, max_poc)) +
   scale_x_log10() +
   annotation_logticks(size = 0.1) +
@@ -54,7 +57,7 @@ p1 <- poc_cp %>%
 # y = 10^a * x^b where a and b are the intercept and the slope
 tibble(
   x = seq(0.006668996, 1.000743797, length.out = 100),
-  y = 10^1.9 * x^0.74
+  y = 10^2.9 * x^0.74
 ) %>%
   ggplot(aes(x, y)) +
   geom_point()+
@@ -63,18 +66,18 @@ tibble(
   annotation_logticks(sides = "l")
 
 poc_cp %>%
-  mutate(pred = 10^1.9 * cp^0.74) %>%
-  ggplot(aes(x = cp, y = poc_umol_l)) +
+  mutate(pred = 10^2.9 * cp^0.74) %>%
+  ggplot(aes(x = cp, y = poc_mg_m3)) +
   geom_point(color = "#393E41") +
   geom_line(aes(y = pred), color = "#bf1d28", size = 0.5)
 
 # POC vs chla -------------------------------------------------------------
 
 p2 <- poc_cp %>%
-  ggplot(aes(x = flor_mg_m3, y = poc_umol_l)) +
+  ggplot(aes(x = flor_mg_m3, y = poc_mg_m3)) +
   geom_point(color = "#393E41") +
-  ggpubr::stat_regline_equation(label.y = log10(80)) +
-  ggpubr::stat_regline_equation(label.y = log10(50), aes(label = ..rr.label..)) +
+  ggpubr::stat_regline_equation(label.y.npc = 1) +
+  ggpubr::stat_regline_equation(label.y.npc = 0.93, aes(label = ..rr.label..)) +
   scale_y_log10(limits = c(min_poc, max_poc)) +
   scale_x_log10() +
   annotation_logticks(size = 0.1) +
@@ -107,10 +110,10 @@ poc_bbp <- poc_bbp %>%
   filter(depth_m <= 100)
 
 p3 <- poc_bbp %>%
-  ggplot(aes(x = bbp, y = poc_umol_l)) +
+  ggplot(aes(x = bbp, y = poc_mg_m3)) +
   geom_point(color = "#393E41") +
-  ggpubr::stat_regline_equation(label.y = log10(80)) +
-  ggpubr::stat_regline_equation(label.y = log10(50), aes(label = ..rr.label..)) +
+  ggpubr::stat_regline_equation(label.y.npc = 1) +
+  ggpubr::stat_regline_equation(label.y.npc = 0.93, aes(label = ..rr.label..)) +
   scale_y_log10(limits = c(min_poc, max_poc)) +
   scale_x_log10(labels = scales::label_number()) +
   annotation_logticks(size = 0.1) +
@@ -141,7 +144,7 @@ ggsave(
 
 # Correlation statistics --------------------------------------------------
 
-cor(log10(poc_cp$cp), log10(poc_cp$poc_umol_l), "complete.obs")
-cor(log10(poc_cp$flor_mg_m3), log10(poc_cp$poc_umol_l), "complete.obs")
-cor(log10(poc_bbp$bbp), log10(poc_bbp$poc_umol_l), "complete.obs")
+cor(log10(poc_cp$cp), log10(poc_cp$poc_mg_m3), "complete.obs")
+cor(log10(poc_cp$flor_mg_m3), log10(poc_cp$poc_mg_m3), "complete.obs")
+cor(log10(poc_bbp$bbp), log10(poc_bbp$poc_mg_m3), "complete.obs")
 
